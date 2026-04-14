41719e3d8a1d47cdf0d9daf92d27abee9796bf28
Julian Sommer <sommerjulian@t-online.de>
2026-04-14 16:06:16 +0200

    done with favorite model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_13_153027) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_14_135948) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.bigint "plant_id", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["plant_id"], name: "index_favorites_on_plant_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "plants", force: :cascade do |t|
+    t.boolean "air_purifying"
+    t.string "care_level"
+    t.datetime "created_at", null: false
+    t.string "growth_style"
+    t.string "indoor_outdoor"
+    t.string "light_needs"
+    t.string "name"
+    t.boolean "pet_safe"
+    t.datetime "updated_at", null: false
+    t.string "water_needs"
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -25,4 +47,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_13_153027) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "favorites", "plants"
+  add_foreign_key "favorites", "users"
 end
