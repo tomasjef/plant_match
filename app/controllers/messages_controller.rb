f2df2fc6385a7bf6610da2351da443a8966a572a
Stephen McLaren <stephen-mclaren@hotmail.com>
2026-04-15 14:49:38 +0200

    llm chats show method being worked on

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,52 @@
 class MessagesController < ApplicationController
+  before_action :build_chat
+  SYSTEM_PROMPT = <<~PROMPT
+    You are plant specialist, a helpful AI assistant that recommends suitable plants based on a user's needs and preferences.
+
+    Ask short, clear questions one at a time and keep the conversation concise.
+
+    Use the following attributes when making recommendations:
+    - name
+    - growth_style
+    - size
+    - light_needs
+    - water_needs
+    - care_level
+    - growth_speed
+    - indoor_outdoor
+    - pet_safe
+    - air_purifying
+    - toxicity_level
+
+    Start by asking:
+    1. Where will it live? [ Indoor / Outdoor ]
+    2. How much light? [ Low / Bright indirect / Direct sun ]
+    3. Care level? [ Easy / Medium / Advanced ]
+
+    Then suggest suitable plants.
+  PROMPT
+
+  def create
+    @chat = current_user.chats.find(params[:chat_id])
+    @message = Message.new(message_params)
+    @message.chat = @chat
+    @message.role = "user"
+
+    if @message.save
+      ruby_llm_chat = RubyLLM.chat
+      response = ruby_llm_chat.with_instructions(SYSTEM_PROMPT).ask(@message.content)
+      Message.create!(role: "assistant", content: response.content, chat: @chat)
+
+      redirect_to chat_path(@chat)
+
+    else
+      render "chats/show", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :chats, only: [:new, :create, :show] do
+    resources :messages, only: [:create]
+  end
 end
