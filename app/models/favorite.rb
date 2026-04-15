046dc47ddf83f114ddb594eb7e4288de8b32648a
Julian Sommer <sommerjulian@t-online.de>
2026-04-15 17:10:44 +0200

    feat: refine favorite model validation, implement favorites/view

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,5 +2,5 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :plant
 
-  validates :plant_id, presence: true, uniqueness: true
+  validates :plant_id, presence: true, uniqueness: { scope: :user_id, message: "is already in your garden" } # without the user_id scope the plant would not be able to be added by another user to favorites
 end
