99384af9fa1b80a3491f2a99b4fa9588bbbaf57b
Marta Gobbo <martagobbo15@gmail.com>
2026-04-16 12:25:43 +0200

    add plant_info caching to avoid repeated LLM calls

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -11,8 +11,9 @@ class PlantsController < ApplicationController
     # I'm using the LLM to generate care tips and a short history for each plant
     # It makes one call and returns both sections together
     # The result goes into @plant_info which I display in the show view
+  if @plant.plant_info.blank?
     response = RubyLLM.chat
-      .with_instructions("You are a plant expert. Be concise. Use ** around section titles to make them bold.")
+      .with_instructions("You are a plant expert. Be concise but informative.")
       .ask("For a #{@plant.name} plant, give me:
       1. Tips to take care of the plant (a paragraph)
       2. A 2-3 sentence history and background on this specific plant
@@ -25,7 +26,8 @@ class PlantsController < ApplicationController
       - Tip two
       - Tip three")
       .content
-
-    @plant_info = response
+    @plant.update(plant_info: response)
+  end
+    @plant_info = @plant.plant_info
   end
 end
